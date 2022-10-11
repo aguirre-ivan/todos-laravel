0abcde0c7af8e12c9c27fc1bbd257980a11d1675
Iván Aguirre <iaguirre@fi.uba.ar>
2022-10-11 14:49:28 -0300

    Added delete category modal

--- todos-project/resources/views/components/_categories_list.blade.php
+++ todos-project/resources/views/components/_categories_list.blade.php
@@ -14,9 +14,43 @@
                     method="POST">
                     @method('DELETE')
                     @csrf
-                    <button type="submit"
+
+                    <!-- Button trigger modal -->
+                    <button type="button"
                         class="btn-close btn-close-white"
+                        data-bs-toggle="modal"
+                        data-bs-target="#deleteModal"
                         aria-label="Delete"></button>
+
+                    <!-- Modal -->
+                    <div class="modal fade text-dark"
+                        id="deleteModal"
+                        tabindex="-1"
+                        aria-labelledby="deleteCategory"
+                        aria-hidden="true">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h1 class="modal-title fs-5"
+                                        id="deleteCategory">Delete category</h1>
+                                    <button type="button"
+                                        class="btn-close"
+                                        data-bs-dismiss="modal"
+                                        aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body">
+                                    Are you sure? Todos will be erased too.
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button"
+                                        class="btn btn-secondary"
+                                        data-bs-dismiss="modal">Cancel</button>
+                                    <button type="submit"
+                                        class="btn btn-primary">Continue</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </form>
             </div>
         </div>
